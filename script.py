dc2ce1e733bc8232a1c05b357d4998bc770d2e24
PedrowjCA <pedrojos.campos@gmail.com>
2024-07-19 12:03:05 -0300

    Creating the functions getUrbanizationRate, getElectricityProd, getGoldProd

--- script.py
+++ script.py
@@ -3,26 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 
-
-'''def getCapitalPopulation(countryName):
-    url = 'https://en.wikipedia.org/wiki/List_of_national_capitals_by_population'
-    page = requests.get(url).text
-
-    soup = BeautifulSoup(page, 'html.parser')
-
-    for item in soup.select('table.wikitable tbody tr td a'):
-        name = item.text.replace("*",'').strip() if '*' in item.text else item.text.strip()
-        next_td = item.parent.find_next_sibling('td')
-        previous_td = item.parent.find_previous_sibling('td')
-
-        if (countryName == name) and ((countryName not in previous_td.get_text()) if previous_td else True):
-            population = next_td.find_next_sibling('td').get_text()
-            population = int(population.replace(',', '')) if (',' in population) else int(population)
-            return population
-
-    return 0'''
-
-
 def getCapitalPopulation(country_name):
     translated_country_name = GoogleTranslator(source='auto', target='english').translate(text=country_name)
     url = 'https://en.wikipedia.org/wiki/List_of_national_capitals_by_population'
@@ -48,14 +28,96 @@ def getCapitalPopulation(country_name):
 
     return 0
 
+def getUrbanizationRate(country_name):
+    translated_country_name = GoogleTranslator(source='auto', target='english').translate(text=country_name)
+    url = 'https://en.wikipedia.org/wiki/Urbanization_by_sovereign_state'
+    try:
+        page = requests.get(url)
+    except:
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for row in soup.select('table.wikitable tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 4:
+            continue
+        
+        country_column = columns[0]
+
+        country = country_column.find('a').text.strip() if country_column.find('a') else False
+        if country == country_name or country == translated_country_name:
+            urbanization_rate = columns[3].get_text()
+            return float(urbanization_rate)
+
+    return 0
+
+def getElectricityProd(country_name):
+    translated_country_name = GoogleTranslator(source='auto', target='english').translate(text=country_name)
+    url = 'https://en.wikipedia.org/wiki/List_of_countries_by_electricity_production'
+    try:
+        page = requests.get(url)
+    except:
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for row in soup.select('table.wikitable tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 2:
+            continue
+        
+        country_column = columns[0]
+
+        if country_column.find('a'):
+            country = country_column.find('a').text.strip()
+            if country == country_name or country == translated_country_name:
+                electricity_prod = columns[1].get_text().replace(',', '')
+                return float(electricity_prod)
+
+    return 0
+
+def getGoldProd(country_name):
+    url = 'https://en.wikipedia.org/wiki/List_of_countries_by_gold_production'
+    try:
+        page = requests.get(url)
+    except:
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for row in soup.select('table.wikitable tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 3:
+            continue
+        
+        country_column = columns[1]
+
+        if country_column.find('a'):
+            country = country_column.find('a').text.strip()
+            if country == country_name:
+                gold_prod = columns[2].get_text().replace(',', '')
+                return int(gold_prod)
+
+    return 0
+
 countries = [
     'China', 
     'Iran',
-    "Côte D'Ivoire"
+    "Côte D'Ivoire",
+    'United States'
 ]
 
 for country in countries:
-    print(f"{country}: {getCapitalPopulation(country)}")
+    print(f'''{country}: capital: {getCapitalPopulation(country)},
+    urbanization_rate: {getUrbanizationRate(country)},
+    electricity_prod: {getElectricityProd(country)},
+    gold_prod: {getGoldProd(country)}
+    ''')
+
 
 '''
 [] Criar uma lista com todos os países
