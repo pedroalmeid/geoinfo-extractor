97f277892589a6848a76d30e5cb2d9ec7f23ea0a
PedrowjCA <pedrojos.campos@gmail.com>
2024-07-19 10:06:35 -0300

    Creating web scrap function GetCapitalPopulation

--- script.py
+++ script.py
@@ -1,12 +1,37 @@
-'''
-Prováveis pacotes necessários:
 import country_converter as coco
-from deep_translator import (GoogleTranslator) 
+import requests
 from bs4 import BeautifulSoup
-import requests as r'''
+from deep_translator import GoogleTranslator
 
 
-countries = ['Brazil']
+def getCapitalPopulation(countryName):
+    url = 'https://en.wikipedia.org/wiki/List_of_national_capitals_by_population'
+    page = requests.get(url).text
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    for item in soup.select('table.wikitable tbody tr td a'):
+        name = item.text.replace("*",'').strip() if '*' in item.text else item.text.strip()
+        next_td = item.parent.find_next_sibling('td')
+        previous_td = item.parent.find_previous_sibling('td')
+
+        if (countryName == name) and ((countryName not in previous_td.get_text()) if previous_td else True):
+            population = next_td.find_next_sibling('td').get_text()
+            population = int(population.replace(',', '')) if (',' in population) else int(population)
+            return population
+
+    return 0
+
+countries = [
+    'Equatorial Guinea',
+    'Guinea',
+    'Guinea Bissau',
+    'Papua New Guinea',
+]
+
+for country in countries:
+    print(getCapitalPopulation(country))
+
 
 '''
 [] Criar uma lista com todos os países
@@ -19,3 +44,4 @@ traduzidos de países em casos específicos
 as categorias e respectivos valores extraídos pelas funções de web scrap
 [] Realizar requisição de criação de país para a API REST para criar o país 
 automaticamente no banco de dados
+'''
