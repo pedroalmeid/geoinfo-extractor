0d07fb240c2af0e3c1d1521d4a31ba0694fd07d0
PedrowjCA <pedrojos.campos@gmail.com>
2024-07-19 21:19:20 -0300

    Creating functions getHighestGeographicalPoint, getImmigrantPercentage and getIntentionalHomicideRate

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import country_converter as coco
 import requests
+import re
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 
@@ -46,7 +47,7 @@ def getUrbanizationRate(country_name):
         
         country_column = columns[0]
 
-        country = country_column.find('a').text.strip() if country_column.find('a') else False
+        country = country_column.find('a').text if country_column.find('a') else False
         if country == country_name or country == translated_country_name:
             urbanization_rate = columns[3].get_text()
             return float(urbanization_rate)
@@ -72,7 +73,7 @@ def getElectricityProd(country_name):
         country_column = columns[0]
 
         if country_column.find('a'):
-            country = country_column.find('a').text.strip()
+            country = country_column.find('a').text
             if country == country_name or country == translated_country_name:
                 electricity_prod = columns[1].get_text().replace(',', '')
                 return float(electricity_prod)
@@ -97,10 +98,92 @@ def getGoldProd(country_name):
         country_column = columns[1]
 
         if country_column.find('a'):
-            country = country_column.find('a').text.strip()
+            country = country_column.find('a').text
             if country == country_name:
                 gold_prod = columns[2].get_text().replace(',', '')
                 return int(gold_prod)
+
+    return 0
+
+def getHighestGeographicalPoint(country_name):
+    portuguese_country_name = GoogleTranslator(source='auto', target='pt').translate(text=country_name)
+    url = 'https://pt.wikipedia.org/wiki/Lista_de_pa%C3%ADses_por_ponto_mais_alto'
+    try:
+        page = requests.get(url)
+    except: 
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for row in soup.select('table.wikitable tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 4:
+            continue
+        
+        country_column = columns[1]
+
+        if country_column.find('a'):
+            for country in country_column.find_all('a'):
+                if country.text == portuguese_country_name:
+                    string = columns[3].get_text().replace(',','.')
+                    re_search = r'[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
+                    numbers_in_string = re.findall(re_search, string)
+                    points = [float(number) for number in numbers_in_string]
+                    highest_point = max(points)
+                    return highest_point
+      
+    return 0
+
+def getImmigrantPercentage(country_name):
+    translated_country_name = GoogleTranslator(source='auto', target='english').translate(text=country_name)
+    url = 'https://en.wikipedia.org/wiki/List_of_sovereign_states_by_immigrant_and_emigrant_population'
+    try:
+        page = requests.get(url)
+    except:
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    for row in soup.select('table.wikitable tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 3:
+            continue
+        
+        country_column = columns[0]
+
+        if country_column.find('a'):
+            country = country_column.find('a').text
+            if country == country_name or country == translated_country_name:
+                immigrant_percentage = columns[2].get_text()
+                return float(immigrant_percentage)
+
+    return 0
+
+def getIntentionalHomicideRate(country_name):
+    translated_country_name = GoogleTranslator(source='auto', target='english').translate(text=country_name)
+    url = 'https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate'
+    try:
+        page = requests.get(url)
+    except:
+        return 0
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    homicides_table = soup.find_all('table')[1]
+
+    for row in homicides_table.select('tbody tr'):
+        columns = row.find_all('td')
+
+        if len(columns) < 2:
+            continue
+        
+        country_column = columns[0]
+        country = country_column.find('a').text.replace("*", '').strip()
+        if country == country_name or country == translated_country_name:
+            homicide_rate = columns[1].get_text()
+            return float(homicide_rate)
 
     return 0
 
@@ -108,16 +191,20 @@ countries = [
     'China', 
     'Iran',
     "Côte D'Ivoire",
-    'United States'
+    'United States',
+    'Germany',
+    'Iraque'
 ]
 
 for country in countries:
     print(f'''{country}: capital: {getCapitalPopulation(country)},
     urbanization_rate: {getUrbanizationRate(country)},
     electricity_prod: {getElectricityProd(country)},
-    gold_prod: {getGoldProd(country)}
+    gold_prod: {getGoldProd(country)},
+    highest_geographical_point: {getHighestGeographicalPoint(country)},
+    immigrant_percentage: {getImmigrantPercentage(country)},
+    intentional_homicidade_rate: {getIntentionalHomicideRate(country)},
     ''')
-
 
 '''
 [] Criar uma lista com todos os países
